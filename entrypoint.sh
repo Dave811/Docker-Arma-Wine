76d92e9608939cbab36ef4d73a83a74e8f545e10
David Haßhoff <d.hasshoff97@gmail.com>
2020-05-26 21:44:01 +0200

    add wineprefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /home/container
 
+WINEPREFIX=~/.wine wineboot
+WINEPREFIX=~/.wine32 WINEARCH=win32 wineboot
+
+WINEPREFIX=~/.wine32 WINEARCH=win32 arma3server.exe
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
