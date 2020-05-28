f5a16429e189575309be23161d350018f307d7bd
David Haßhoff <d.hasshoff97@gmail.com>
2020-05-28 15:46:33 +0200

    fix try something

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ cd /home/container
 export DISPLAY=:20
 Xvfb :20 -screen 0 1366x768x16 &
 x11vnc -passwd TestVNC -display :20 -N -forever &
-wait
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
